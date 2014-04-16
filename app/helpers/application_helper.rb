6475fc819cc46b792939673f32c461134cdc397a
juan <juan@juanpc.(none)>
2014-04-16 10:40:21 +0200

    Add contact page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# Mostrar el titulo de la pagina por defecto
+	def full_title(page_title)
+		base_title="Gestión de Calendarios Laborales"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}  "
+		end
+	end 
+
 end
