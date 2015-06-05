d31c40f4bfb8da2a1a73f8a07367ba317f8f8213
jcmorenoruiz <jcmorenoruiz@gmail.com>
2015-06-05 17:59:17 +0200

    Add language to datatables

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+var language =
+ {
+        "lengthMenu": "Mostrar _MENU_ registros por página",
+              "zeroRecords": "No se encontraron resultados",
+              "info": "Mostrando página _PAGE_ de _PAGES_  con _TOTAL_ registros ",
+              "infoEmpty": "No hay registros disponibles",
+              "infoFiltered": "(filtrados de _MAX_  registros totales)",
+              "paginate": {
+                  "first":      "Primera",
+                  "previous":   "Anterior",
+                  "next":       "Siguiente",
+                  "last":       "Última"
+                },
+                "search": "Buscar:",
+                "emptyTable": "No hay datos que coincidan con la búsqueda en la tabla"
+}
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -46,19 +46,3 @@ var ready = function() {
 $(document).ready(ready);
 $(document).on('page:load', ready);
 
-var language =
- {
-        "lengthMenu": "Mostrar _MENU_ registros por página",
-              "zeroRecords": "No se encontraron resultados",
-              "info": "Mostrando página _PAGE_ de _PAGES_  con _TOTAL_ registros ",
-              "infoEmpty": "No hay registros disponibles",
-              "infoFiltered": "(filtrados de _MAX_  registros totales)",
-              "paginate": {
-                  "first":      "Primera",
-                  "previous":   "Anterior",
-                  "next":       "Siguiente",
-                  "last":       "Última"
-                },
-                "search": "Buscar:",
-                "emptyTable": "No hay datos que coincidan con la búsqueda en la tabla"
-}
